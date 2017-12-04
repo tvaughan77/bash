3212f13a7861fbd370eb30c39b04331d53671312
Thomas Vaughan <tom@upside-services.com>
2017-12-04 17:27:10 -0500

    adding terraform to path, fixing some aliases

--- myshell/aliases.bash
+++ myshell/aliases.bash
@@ -1,2 +1,3 @@
-alias uuid="curl -q https://www.uuidgenerator.net/api/version4 2>/dev/null | pbcopy && pbpaste"
+alias uuid="curl -q https://www.uuidgenerator.net/api/version4 2>/dev/null | tr -d '[:space:]' | pbcopy && pbpaste && echo"
 alias xml="xmllint --format -"
+alias tsv="pbpaste | sed 's/|//g' | sed 's/^ *//' | sed 's/ *$//' | sed 's/   */   /g'  | grep -v "^[+-][+-]*$" | pbcopy"
--- myshell/upside.bash
+++ myshell/upside.bash
@@ -1,6 +1,9 @@
 # Upside settings
 export UPSIDE_MAVEN_USER=tvaughan
 
+# Downloaded from https://www.terraform.io/downloads.html 
+export PATH=$PATH:/Users/tvaughan/workspace/terraform
+
 # shell out to our gateway server
 alias gw="ssh -i ~/.ssh/upside-us-east-1-gateway-20170131.pem ec2-user@ec2-54-208-64-212.compute-1.amazonaws.com"
 
