ae813b84a64bbc23d283b210086f5fc4b83b1218
Thomas Vaughan <tom@upside-services.com>
2019-01-14 15:58:10 -0500

    cleaning up python

--- myshell/python.bash
+++ myshell/python.bash
@@ -1,11 +1,12 @@
-alias ve="virtualenv env && source env/bin/activate"
-alias json="python -mjson.tool"
-alias fixpy="pbpaste | sed 's/^>>> //' | sed 's/^... //' | pbcopy"
-
 # Initializes pyenv-virtualenv if pyenv is installed
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
-#eval "$(pyenv virtualenv-init -)"
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 pyenv virtualenvwrapper
+
+alias ve="virtualenv env && source env/bin/activate"
+alias json="python -mjson.tool"
+alias fixpy="pbpaste | sed 's/^>>> //' | sed 's/^... //' | pbcopy"
+
+
